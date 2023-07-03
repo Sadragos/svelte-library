67b7f76ac4c6d5ad4bef8f2168e48861c5834eb3
Kevin Fehlemann <fehlemann@geoinformationsdienst.de>
2023-07-03 13:55:10 +0200

    Bugfixes

---
+++ components/controls/smartActionBar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { t } from 'svelte-i18n';
+	import SmartButton, { type ClickAction } from './smartButton.svelte';
+
+	export let isNew: boolean = false;
+	export let hideDelete: boolean = false;
+	export let deleteClick: ClickAction = () => undefined;
+	export let confirmDeleteText: string = $t('confirmDelete');
+	export let saveClick: ClickAction;
+</script>
+
+<div class="input-group">
+	{#if !isNew && !hideDelete}
+		<SmartButton
+			confirmClick
+			buttonClass="btn-danger"
+			icon="delete"
+			onClick={deleteClick}
+			dialogHeader={$t('confirmDelete')}
+			dialogText={confirmDeleteText}
+			confirmIcon="delete"
+			confirmButtonClass="btn-danger"
+		/>
+	{/if}
+
+	<SmartButton buttonClass="btn-success w-100" icon="save" onClick={saveClick} containerClass="flex-grow-1"/>
+</div>
